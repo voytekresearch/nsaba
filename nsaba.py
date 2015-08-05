4de1145f262e5f8cca29770c58a98d1c066f68d4
Simon Haxby <simon.haxby@gmail.com>
2015-08-04 21:00:12 -0700

    less butchering more doing

--- nsaba.py
+++ nsaba.py
@@ -6,34 +6,24 @@ Authors: Simon Haxby, Scott Susi, Torben Noto
 Last Updated: 7/26/2015
 """
 import numpy as np
-import scipy as sp
 import pandas as pd
 import os
 import itertools
 
+from scipy import spatial
+
 
 class Nsaba(object):
     aba = {
-        'exp_mat': None,
         'exp_df': None,
-        'probe_mat': None,
         'probe_df': None,
-        'si_mat': None,
         'si_df': None,
         'mni_coords': None
     }
 
     ns = {
-        'mni_term_table': None,
-        'terms': None,
-        'study_ids': None,
-        'unique_ids': None,
-        'id_x_features': None,
-        'id_dict': None,
-        'database_labels': None,
-        'x_mni': None,
-        'y_mni': None,
-        'z_mni': None,
+        'database_df': None,
+        'features_df': None,
     }
 
     @classmethod
@@ -52,7 +42,6 @@ class Nsaba(object):
 
         csv_path = os.path.join(aba_path, csv_names[1])
         cls.aba['si_df'] = pd.read_csv(csv_path)
-        cls.aba['si_mat'] = cls.aba['si_df'].as_matrix()
         print '%s loaded.' % csv_names[1]
 
         csv_path = os.path.join(aba_path, csv_names[0])
@@ -61,19 +50,16 @@ class Nsaba(object):
             itertools.chain.from_iterable(
                 [['probe_id'], range(cls.aba['si_df'].shape[0])]))
 
-        cls.aba['exp_mat'] = cls.aba['exp_df'].as_matrix()
         print '%s loaded.' % csv_names[0]
 
         csv_path = os.path.join(aba_path, csv_names[2])
         cls.aba['probe_df'] = pd.read_csv(csv_path)
-        cls.aba['probe_mat'] = cls.aba['probe_df'].as_matrix()
         print '%s loaded.' % csv_names[2]
 
-        raw_coords = cls.aba['si_mat'][1:, 10:].astype(float)
-        cls.aba['mni_coords'] = sp.spatial.KDTree(raw_coords)
+        raw_coords = cls.aba['si_df'].loc[:, 'mni_x':'mni_z'].as_matrix().astype(float)
+        cls.aba['mni_coords'] = spatial.KDTree(raw_coords)
         print "Nsaba.aba['mni_coords'] initialized."
 
-        return 0
 
     @classmethod
     def ns_load(cls, ns_path=".", ns_files=None):
@@ -82,33 +68,11 @@ class Nsaba(object):
             ns_files = ['database.txt', 'features.txt']
 
         df = pd.read_table(os.path.join(ns_path, ns_files[0]))
-        mni_space = df[df['space'].str.contains("MNI")]
+        cls.ns['database_df'] = df.loc[df.space == 'MNI', ['id', 'x', 'y', 'z']]
+        print "%s loaded." % ns_files[0]
 
-        cls.ns['x_mni'] = list(mni_space['x'])
-        cls.ns['y_mni'] = list(mni_space['y'])
-        cls.ns['z_mni'] = list(mni_space['z'])
-        cls.ns['study_ids'] = list(mni_space['id'])
-        cls.ns['unique_ids'] = list(set(cls.ns['study_ids']))  # removing duplicates
-        print '%s keys loaded.' % ns_files[0]
-
-        term_table = pd.read_table(os.path.join(ns_path, ns_files[1]))
-        cls.ns['mni_term_table'] = term_table.loc[term_table['pmid'].isin(cls.ns['unique_ids'])]
-        cls.ns['terms'] = term_table.columns.values
-        cls.ns['id_x_features'] = np.array(cls.ns['mni_term_table']['pmid'])
-        cls.ns['database_labels'] = df.columns.values
-        print '%s keys loaded.' % ns_files[1]
-
-        c = 0
-        cls.ns['id_dict'] = {}
-        for i in cls.ns['study_ids']:
-            if i not in cls.ns['id_dict']:
-                cls.ns['id_dict'][i] = [(cls.ns['x_mni'][c], cls.ns['y_mni'][c], cls.ns['z_mni'][c])]
-                c += 1
-            elif (cls.ns['x_mni'][c], cls.ns['y_mni'][c], cls.ns['z_mni'][c]) in cls.ns['id_dict'][i]:
-                c += 1
-            else:
-                cls.ns['id_dict'][i].append((cls.ns['x_mni'][c], cls.ns['y_mni'][c], cls.ns['z_mni'][c]))
-        return 0
+        cls.ns['features_df'] = pd.read_table(os.path.join(ns_path, ns_files[1]))
+        print "%s loaded." % ns_files[1]
 
     def __init__(self):
 
@@ -141,63 +105,38 @@ class Nsaba(object):
             ge_df = self.aba['exp_df'].loc[self.aba['exp_df']['probe_id'].isin(probe_ids)]
             ge_mat = ge_df.as_matrix().astype(float)[:, 1:].T
 
-            self.ge[entrez_id] = []
+            #self.ge[entrez_id] = []
 
-            self.ge[entrez_id].append(np.mean(ge_mat, axis=1))
-            diff_arr = [np.amax(row) - np.amin(row) for row in ge_mat]
-            self.ge[entrez_id].append(diff_arr)
-            self.ge[entrez_id].append(np.amax(diff_arr))
-            self.ge[entrez_id].append(len(ge_mat[0]))
-            self.ge[entrez_id] = np.squeeze(self.ge[entrez_id])
+            self.ge[entrez_id] = np.mean(ge_mat, axis=1)
 
-        return 0
+            # Additional Metrics
+            # self.ge[entrez_id].append(np.mean(ge_mat, axis=1))
+            # diff_arr = [np.amax(row) - np.amin(row) for row in ge_mat]
+            # self.ge[entrez_id].append(diff_arr)
+            # self.ge[entrez_id].append(np.amax(diff_arr))
+            # self.ge[entrez_id].append(len(ge_mat[0]))
+            # self.ge[entrez_id] = np.squeeze(self.ge[entrez_id])
 
-    def get_aba_xyz(self):
-        xvals = self.aba['mni_coords']
-        yvals = self.aba['si_df']['mni_y']
-        zvals = self.aba['si_df']['mni_z']
-        self.aba['xyz'] = zip(xvals, yvals, zvals)
-        return self.aba['xyz']
 
-    # NSbook keeping methods
+    # NeuroSynth book-keeping methods
+
     def is_term(self, term):
-        """Checks if this term is in the neurosynth database"""
-        if term in self.ns['mni_term_table']:
+        """ Checks if this term is in the neurosynth database """
+        if term in self.ns['features_df'].columns:
             return True
         else:
             return False
 
-    def is_location(self, coords):
-        """Checks if coordinate set (x,y,z) is mentioned in any studies"""
-        if np.floor(coords[0]) in np.floor(self.ns['x_mni']):
-            # ind = x_vals.index(coords[0]);
-
-            for xind in xrange(len(self.ns['x_mni'])):
-                if coords[0] == self.ns['x_mni'][xind]:
-                    if np.floor(self.ns['y_mni'][xind]) == np.floor(coords[1]):
-                        # print '2'
-                        if np.floor(self.ns['z_mni'][xind]) == np.floor(coords[2]):
-                            return True
-            else:
-                return False
-        else:
-            return False
-
     def is_id(self, ID):
-        """Checks if ID is an ID with NMI coordinates"""
-        if ID in self.ns['id_dict']:
+        """Checks if ID is registered"""
+        if (self.ns['features_df']['pmid'] == ID).any():
             return True
         else:
             return False
 
     def coord_to_ids(self, coord):
         """Uses the study dictionary above to find study ids from x,y,z coordinates"""
-        if self.is_location(coord):
-            self.ns['temp_IDs'] = []
-            for i, coords in self.ns['id_dict'].items():
-                if coord in coords:
-                    self.ns['temp_IDs'].append(i)
-            return self.ns['temp_IDs']
+        # Use
         else:
             return "These coordinates don't match any studies"
 
@@ -207,6 +146,7 @@ class Nsaba(object):
             ind = int(np.squeeze(np.where(self.ns['id_x_features'] == ID)[0]))
             self.ns['temp_term'] = list(self.ns['mni_term_table'].iloc[ind][1:])
             return self.ns['temp_term']
+
         else:
             return 'Not an ID of a study in NMI space'
 
@@ -249,8 +189,6 @@ class Nsaba(object):
         ids = self.term_to_ids(term, thresh)
         self.ns['temp_coords'] = [self.id_to_coords(i) for i in ids]
         return self.ns['temp_coords']
-
-    def
 
     # estimating term weights of unknown location
     def term_vector_of_unknown_point(self, coord, maxDist):
