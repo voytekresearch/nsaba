8ab481721d94fba94f2269df9ed434d2987f2b5d
Simon Haxby <simon.haxby@gmail.com>
2015-10-05 18:47:01 -0700

    analysis group splitting methods added, +(small tweaks)

--- nsaba/analysis.py
+++ nsaba/analysis.py
@@ -12,7 +12,10 @@ from scipy import stats
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import mixture
 import csv
+import time
 
 
 def cohen_d(x1, x2, n1, n2):
@@ -30,48 +33,83 @@ class NsabaAnalysis(object):
         self.gene_rec = collections.namedtuple("gene_rec", "entrez cohen_d p_value")
         print "To use inline plotting functionality in Jupyter, '%matplotlib inline' must be enabled"
 
-    def t_test(self, term, gene, quant=None, log=False, graphops='density'):
+    def _split_mask(self, term_vec, method='var', **kwargs):
+        """ Constructs splitting mask via specified method"""
+        if method == 'var':
+            if 'quant' in kwargs:
+                thres = np.percentile(term_vec, kwargs['quant'])
+                mask = np.array([True if coeff > thres else False for coeff in term_vec])
+                return mask
+            else:
+                raise KeyError("quant parameter never set in conjunction with 'var' method")
+        elif method == 'kmeans':
+            X = [[x] for x in term_vec]
+            init_clusters = np.array([[min(term_vec)], [max(term_vec)]])
+            kmn = KMeans(n_clusters=2, init=init_clusters)
+            mask = kmn.fit_predict(X)
+            return mask.astype(bool)
+        elif method == 'mog':
+            X = [[x] for x in term_vec]
+            gm = mixture.GMM(n_components=2)
+            y = gm.fit(X)
+            if stats.mode(y)[0][0] == 0:
+                mask = np.array([True if gm.predict([coeff]) == 1 else False for coeff in term_vec])
+            else:
+                mask = np.array([False if gm.predict([coeff]) == 1 else True for coeff in term_vec])
+            return mask
+        else:
+            raise ValueError("method parameter '%s' not recognized")
+
+    def _split_groups(self, ge_vec, mask):
+        """Splits gene samples into control and functional network"""
+        functional_grp = ge_vec[mask]
+        diff = set(ge_vec) - set(functional_grp)
+        control_grp = np.array(list(diff))
+        return control_grp, functional_grp
+
+    def t_test(self, term, gene, split_method='var', quant=None, log=False, graphops='density'):
         """ T-Test of gene expression between term and non-term coordinates"""
-        if not quant:
+        if not quant and split_method == 'var':
             quant = 85
         analmat = self.no.make_ge_ns_mat(term, [gene])
-        thres = np.percentile(analmat[:, 1], quant)
         # Splitting groups
-        gt_thres = [row[0] for row in analmat if row[1] > thres]
-        lt_thres = [row[0] for row in analmat if row[1] <= thres]
+        mask = self._split_mask(analmat[:, 1], method=split_method, quant=quant)
+        cont_grp, funct_grp = self._split_groups(analmat[:, 0], mask)
         if log:
-            gt_thres= np.log(gt_thres)
-            lt_thres= np.log(lt_thres)
+            funct_grp = np.log(funct_grp)
+            cont_grp = np.log(cont_grp)
 
         # T-Test
-        print "t-value: %.4f \np-value: %.3E" % stats.ttest_ind(lt_thres, gt_thres)
-        print "Effect size: %.4f \n" % cohen_d(lt_thres, gt_thres, len(lt_thres), len(gt_thres))
+        print "t-value: %.4f \np-value: %.3E" % stats.ttest_ind(cont_grp, funct_grp)
+        print "Effect size: %.4f \n" % cohen_d(cont_grp, funct_grp, len(cont_grp), len(funct_grp))
         # Histogram/KDE Plots
         if graphops == 'density':
             ax = plt.axes()
             ax.set_title('Gene Expression Distributions')
             ax.set_xlabel('gene expression')
             ax.set_ylabel('density')
-            sns.distplot(gt_thres, ax=ax, label=term)
-            sns.distplot(lt_thres, label='null')
+            sns.distplot(funct_grp, ax=ax, label=term)
+            sns.distplot(cont_grp, label='null')
             plt.legend()
         elif graphops == 'box':
             ax = plt.axes()
-            ax.boxplot([lt_thres, gt_thres])
+            ax.boxplot([cont_grp, funct_grp])
             ax.set_xticks([1, 2])
             ax.set_xticklabels(['Low '+term, 'High '+term])
         elif graphops == 'violin':
             ax = plt.axes()
-            ax.violinplot([lt_thres, gt_thres])
+            ax.violinplot([cont_grp, funct_grp])
             ax.set_xticks([1, 2])
             ax.set_xticklabels(['Low '+term, 'High '+term])
             ax.set_ylabel('Gene Expression')
-            ax.plot(np.ones(len(lt_thres)), lt_thres, 'b.')
-            ax.plot(1, np.mean(lt_thres), 'bs')
-            ax.plot(2*np.ones(len(gt_thres)), gt_thres, 'g.')
-            ax.plot(2, np.mean(gt_thres), 'gs')
+            ax.plot(np.ones(len(cont_grp)), cont_grp, 'b.')
+            ax.plot(1, np.mean(cont_grp), 'bs')
+            ax.plot(2*np.ones(len(funct_grp)), funct_grp, 'g.')
+            ax.plot(2, np.mean(funct_grp), 'gs')
+        else:
+            raise ValueError("graphops parameter '%s' not recognized" % graphops)
 
-    # @not_operational
+    @not_operational
     def t_test_custom_ge(self, coords1, coords2, gene, quant=None, log=False, graphops='density'):
         """ T-Test of gene expression between term and non-term coordinates"""
         if not quant:
@@ -110,8 +148,10 @@ class NsabaAnalysis(object):
             ax.plot(1, np.mean(ge1), 'bs')
             ax.plot(2*np.ones(len(ge2)), ge2, 'g.')
             ax.plot(2, np.mean(ge2), 'gs')
+        else:
+            raise ValueError("graphops parameter '%s' not recognized" % graphops)
 
-    # @not_operational
+    @not_operational
     def t_test_custom_term(self, coords1, coords2, term, quant=None, log=False, graphops='density'):
         """ T-Test of gene expression between term and non-term coordinates"""
         if not quant:
@@ -150,37 +190,36 @@ class NsabaAnalysis(object):
             ax.plot(1, np.mean(term1), 'bs')
             ax.plot(2*np.ones(len(term2)), term2, 'g.')
             ax.plot(2, np.mean(term2), 'gs')
-
+        else:
+            raise ValueError("graphops parameter '%s' not recognized" % graphops)
 
     @preprint('This may take a couple of minutes ...')
-    def t_test_multi(self, term, quant=None, sample_num=None, genes_of_interest=[]):
+    def t_test_multi(self, term, quant=None, sample_num=None, split_method='var', genes_of_interest=[]):
         if term not in self.no.term:
             raise ValueError("Term activation not generated for '%s" % term)
         if not sample_num:
             sample_num = len(self.no.ge.keys())
         elif sample_num <= 0:
             raise ValueError("'sample_num' parameter must be greater than 0")
-        if not quant:
+        if not quant and split_method == 'var':
             quant = 85
 
-        thres = np.percentile(self.no.term[term]['ns_act_vector'], quant)
 
         if len(self.no.ge) < sample_num:
             raise ValueError("Sample number exceeds stored number of Entrez IDs")
         sam_ids = random.sample(self.no.ge.keys(), sample_num)
         ge_mat = self.no.make_ge_ns_mat(term, sam_ids).T[:-1]
         term_act_vector = self.no.make_ge_ns_mat(term, sam_ids).T[-1:]
-        loc_num = len(ge_mat[0])
 
-        ttest_metrics = {'term': term, "quantile": quant, "gene_sample_size": sample_num}
+        mask = self._split_mask(term_act_vector, method=split_method, quant=quant)
+
+        ttest_metrics = {'term': term, 'split_method': split_method, "quantile": quant, "gene_sample_size": sample_num}
         gene_stats = []
         for eid, ge in zip(sam_ids, ge_mat):
             # Split coordinates in to term and non-term groups
-            #print term_act_vector
-            gt_thres = [ge[i] for i in xrange(loc_num) if term_act_vector[0][i] > thres]
-            lt_thres = [ge[i] for i in xrange(loc_num) if term_act_vector[0][i] <= thres]
-            test_stats = stats.ttest_ind(lt_thres, gt_thres)
-            d = cohen_d(lt_thres, gt_thres, len(lt_thres), len(gt_thres))
+            cont_grp, funct_grp = self._split_groups(ge, mask)
+            test_stats = stats.ttest_ind(cont_grp, funct_grp)
+            d = cohen_d(cont_grp, funct_grp, len(cont_grp), len(funct_grp))
             # One-sided T-Test
             if test_stats[0] <= 0:
                 gene_stats.append(self.gene_rec(eid, d, test_stats[1]))
@@ -200,7 +239,7 @@ class NsabaAnalysis(object):
         """Prints: ID, p-value, Cohen's d, gene description for genes with the largest effect sizes"""
         top_genes = []
         for rec in ttest_metrics['results'][:nih_fetch_num]:
-            sleep(.5)
+            time.sleep(.5)
             try:
                 gene_dat = get_gene_info(gene_path, str(int(rec.entrez)))
                 gene_name = gene_dat[1]
